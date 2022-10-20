7740746f67fbd516d3efc5d4cbee98d8c3b10bd6
Lisandro Francucci <lisandro.francucci@gmail.com>
2022-10-20 18:56:18 -0300

    Add Pop-Up for tickets form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 function contentLoaded() {
 
+  let tickets = document.getElementById("tickets");
+
   // Capturar los elementos del DOM
   let cantidad = document.getElementById("cantidad");
   let categoria = document.getElementById("categoria");
@@ -17,14 +19,14 @@ function contentLoaded() {
     
     let totalConDescuento;
     if(categoria.value == "estudiante") {
-      // Estudiantes: se descuenta 80%
-      totalConDescuento = (ticketValorTotal - (ticketValorTotal * .8)) * cantidad.value;
+      // Estudiantes: se descuenta 80% (el valor del ticket es el 20% del original)
+      totalConDescuento = ticketValorTotal * cantidad.value * .2; 
     } else if(categoria.value == "trainee") {
-      // Trainee: se descuenta 50%
-      totalConDescuento = (ticketValorTotal - (ticketValorTotal * .5)) * cantidad.value;
+      // Trainee: se descuenta 50% (el valor del ticket es el 50% del original)
+      totalConDescuento = ticketValorTotal * cantidad.value * .5; 
     } else if(categoria.value == "junior") {
-      // Junior: se descuenta 20%
-      totalConDescuento = (ticketValorTotal - (ticketValorTotal * 0.2)) * cantidad.value;
+      // Junior: se descuenta 20% (el valor del ticket es el 80% del original)
+      totalConDescuento = ticketValorTotal * cantidad.value * .8; 
     } else {
       // Si no se seleccionó ningún descuento:
       totalConDescuento = 200 * cantidad.value;
@@ -35,4 +37,33 @@ function contentLoaded() {
 
   btnResumen.addEventListener("click", aplicarDescuento);
 
+  // Mostrar popup al comprar los tickets
+  let discountForm = document.getElementById("discount-form");
+  let required = document.getElementById("discount-form").querySelectorAll("[required]");
+
+  let popup = document.getElementById("popup");
+  let btnComprar = document.getElementById("btn-comprar");
+  let btnCerrar = document.getElementById("btn-cerrar");
+
+  btnComprar.addEventListener("click", function(e) {
+    let valid = true;
+    required.forEach(function(i) {
+      if(!i.value) {
+        valid = false;
+        return;
+      } 
+    });
+    if(valid) {
+      popup.classList.add("active"); 
+    } else {
+      alert("Por favor, completá todos los campos");
+    }
+  });
+
+  btnCerrar.addEventListener("click", function(e) {
+    // Escondo el Pop-Up
+    popup.classList.remove("active");
+    // Elimino los datos del formulario
+    discountForm.reset();
+  })
 }
